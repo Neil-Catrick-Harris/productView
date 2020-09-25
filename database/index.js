01a1dd8f3d551bd847cf20d9614f56ee48e9bb1b
Ahmed-Elawad <a.elawad97@gmail.com>
2020-09-25 00:20:40 -0400

    Implemented document Schema

--- database/index.js
+++ database/index.js
@@ -7,3 +7,47 @@ const options =  {
 const ProductData = mongoose.createConnection('mongodb://localhost/product_data', options);
 
 
+// define document schema
+const itemSchema = new mongoose.schema({
+    Id: Number,
+    Name: String,
+    Description: Text,
+    Details: {
+        materialsCare: [String],
+        Sustainability: [String],
+        Packaging: {
+            Width: Number,
+            Height: Number,
+            Length: Number,
+            Weight: Number,
+            Packages: Number,
+            ArticleNumber: String,
+            Description: String
+        }
+    },
+    imagesUrl: [String],
+    options: {
+        threadCount: [ Number, String],
+        PillowCaseQuantitiy: Number,
+        DuvetCoverLength: [Number, String],
+        DuvorCoverWidth: {
+            size: Number,
+            metric: String
+        },
+        PillowCaseLength:{
+            size: Number,
+            metric: String
+        },
+        PillowCaseWidtch: {
+            size: Number,
+            metric: String
+        },
+        Colors: [String]
+    }
+});
+
+// define document model name
+const Item = mongoose.model('item', itemSchema);
+
+
+
