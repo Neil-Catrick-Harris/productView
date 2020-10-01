27ff05744d779aa8ea079efba0a000859a2c5f67
Ahmed-Elawad <a.elawad97@gmail.com>
2020-09-30 20:20:26 -0400

    Implemented generic materials and care module w/ basic click functionality

---
+++ client/src/components/MaterialsCare.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class ProductDetails extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            productInformation: ['testtest', 'testtest', 'testtest', 'testtest', 'testtest'],
+            clicked: false
+        };
+        this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            clicked: !this.state.clicked
+        });
+        console.log(this.state.clicked);
+    }
+    render() {
+        return (
+            <div id="materialscare header">
+                <div id='materialscare title' onClick={() => this.handleClick()} >Matrials &amp; care</div>
+                <a className='showmaterials arrow' />
+                <div id='materialscare info'>
+                    {this.state.productInformation.map((snippet, index) => {
+                        return (<div key={snippet + index}>{snippet}</div>);
+                    })}
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default ProductDetails;
