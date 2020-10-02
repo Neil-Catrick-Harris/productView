269ce86ab17847ecea9faf9d738aeed1c43819d6
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-02 10:52:22 -0400

    Incorperated modal & product details to main view

--- client/src/components/ProductDetails.jsx
+++ client/src/components/ProductDetails.jsx
@@ -2,23 +2,32 @@ import React from 'react';
 import MaterialsCare from './MaterialsCare.jsx';
 import Sustainibility from './Sustainability.jsx';
 import Packaging from './Packaging.jsx';
+import Modal from './Modal.jsx';
 
 class ProductDetails extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            clicked: false
+            clicked: false,
+            header: 'Product details',
         };
     }
 
+    handleClick() {
+        this.setState({ clicked: !this.state.clicked});
+    }
     render() {
         return (
             <div className='productDetails'>
-                <div id='productDetails  mainDisplay'>Product Details</div>
+                <div  onClick={() => this.handleClick()}  id='productDetails  mainDisplay'>Product Details</div>
                 <a id='arrow'/>
-                <div>
-                    <div>Generic description</div>
-                </div>
+                <Modal 
+                show={this.state.clicked}
+                Content={[
+                    { section: <div>{this.state.header}</div>},
+                    { section: <div>{this.props.product.description}<br /><br /></div>}
+                ]}
+                />
             </div>
         )
     }
