6bcff333b90ce80cd70215207e7306ef4bcfeb0c
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-02 13:31:13 -0400

    Carousel to render on click w/ image centered

--- client/src/components/ImageGrid.jsx
+++ client/src/components/ImageGrid.jsx
@@ -20,7 +20,6 @@ class ImageGrid extends React.Component {
     }
 
     handleImageClick() {
-        debugger;
         this.setState({clicked: !this.state.clicked});
     }
     render() {
--- client/src/components/Imagecarousel.jsx
+++ client/src/components/Imagecarousel.jsx
@@ -1,4 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const CarouselModal = styled.div`
+    position: absolute;
+    background: black;
+    min-width: 100%;
+    min-height: 100%;
+    background: white;`;
+
+const CarouselImageWrapper = styled.div`
+   margin: 25% 25% 25% 25% `;
+
+
+const CarouselImage = styled.img`
+    display: block;
+    padding: 2px;
+    border: solid silver 1px;`;
 
 class ImageCarousel extends React.Component {
     constructor(props) {
@@ -16,16 +33,11 @@ class ImageCarousel extends React.Component {
     }
     render() {
         return (
-            <div className="imageCarousel">
-                {this.state.images.map((imageUrl, index) => {
-                    return (
-                        <div>
-                            <a className="previousImage"/>
-                            <img src={imageUrl} key={imageUrl + index}/>
-                        </div>
-                    );
-                })}
-            </div>
+            <CarouselModal>
+                <CarouselImageWrapper>
+                {<CarouselImage src={this.state.images[2]} key={'asjenfvjandjnvdjwanvj'} />}
+                </CarouselImageWrapper>
+            </CarouselModal>
         );
     }
 }
