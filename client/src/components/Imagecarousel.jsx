cd8e169ba146288141e4bc82377fe97fbae09a37
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-02 14:38:00 -0400

    Image centered w/ surrounding arrows

--- client/src/components/Imagecarousel.jsx
+++ client/src/components/Imagecarousel.jsx
@@ -1,21 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import {ArrowIosBackOutline} from '@styled-icons/evaicons-outline';
+import {ArrowIosForwardOutline} from '@styled-icons/evaicons-outline';
 
 const CarouselModal = styled.div`
+    display: grid;
     position: absolute;
     background: black;
     min-width: 100%;
     min-height: 100%;
-    background: white;`;
+    background: white;
+    grid-template-areas: 
+        'backArrow image forwardArrow'
+        'backArrow image forwardArrow'
+        'backArrow image forwardArrow'`;
 
 const CarouselImageWrapper = styled.div`
-   margin: 25% 25% 25% 25% `;
-
+    grid-area: image;
+    height: 100%;`;
 
 const CarouselImage = styled.img`
     display: block;
     padding: 2px;
+    align: center;
+    margin: 50% 0 50% 0;
     border: solid silver 1px;`;
+
+const BackArrow = styled.div`
+    grid-area: backArrow;
+    position; absolute;
+    margin: auto;`;
+
+const ForwardArrow = styled.div`
+    grid-area: forwardArrow;
+    position; absolute;
+    margin: auto;`;
 
 class ImageCarousel extends React.Component {
     constructor(props) {
@@ -34,9 +53,15 @@ class ImageCarousel extends React.Component {
     render() {
         return (
             <CarouselModal>
+                <BackArrow>
+                    <ArrowIosBackOutline size='48'/>
+                </BackArrow>
                 <CarouselImageWrapper>
-                {<CarouselImage src={this.state.images[2]} key={'asjenfvjandjnvdjwanvj'} />}
+                    {<CarouselImage src={this.state.images[2]} key={'asjenfvjandjnvdjwanvj'} />}
                 </CarouselImageWrapper>
+                <ForwardArrow>
+                    <ArrowIosForwardOutline size='48'/>
+                </ForwardArrow>
             </CarouselModal>
         );
     }
