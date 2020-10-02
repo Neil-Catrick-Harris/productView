41b88ab273fe803a99d08fdc12f2b3664f28887d
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-02 16:04:54 -0400

    Minor sytax changes

--- client/src/components/Imagecarousel.jsx
+++ client/src/components/Imagecarousel.jsx
@@ -13,28 +13,33 @@ const CarouselModal = styled.div`
     grid-template-areas: 
         'backArrow image forwardArrow'
         'backArrow image forwardArrow'
-        'backArrow image forwardArrow'`;
+        'backArrow image forwardArrow'
+`;
 
 const CarouselImageWrapper = styled.div`
     grid-area: image;
-    height: 100%;`;
+    height: 100%;
+`;
 
 const CarouselImage = styled.img`
     display: block;
     padding: 2px;
     align: center;
     margin: 50% 0 50% 0;
-    border: solid silver 1px;`;
+    border: solid silver 1px;
+`;
 
 const BackArrow = styled.div`
     grid-area: backArrow;
     position; absolute;
-    margin: auto;`;
+    margin: auto;
+`;
 
 const ForwardArrow = styled.div`
     grid-area: forwardArrow;
     position; absolute;
-    margin: auto;`;
+    margin: auto;
+`;
 
 class ImageCarousel extends React.Component {
     constructor(props) {
@@ -45,7 +50,6 @@ class ImageCarousel extends React.Component {
             currentImage: null
         };
     }
-
     componentDidMount() {
         this.setState({
             images: this.props.images,
@@ -55,15 +59,11 @@ class ImageCarousel extends React.Component {
     }
     viewPrevious() {
         if (this.state.currentImage === 0) return;
-        this.setState({
-            currentImage: this.state.currentImage - 1
-        })
+        this.setState({currentImage: this.state.currentImage - 1})
     }
     viewNext() {
         if (this.state.currentImage === this.state.numOfImages - 1) return;
-        this.setState({
-            currentImage: this.state.currentImage + 1
-        })
+        this.setState({currentImage: this.state.currentImage + 1})
     }
     render() {
         return (
@@ -72,10 +72,7 @@ class ImageCarousel extends React.Component {
                     <ArrowIosBackOutline size='48'/>
                 </BackArrow>
                 <CarouselImageWrapper>
-                    {<CarouselImage 
-                    src={this.state.images[this.state.currentImage]} 
-                    key={'asjenfvjandjnvdjwanvj'}
-                    />}
+                    {<CarouselImage src={this.state.images[this.state.currentImage]} key={'asjenfvjandjnvdjwanvj'} />}
                 </CarouselImageWrapper>
                 <ForwardArrow onClick={() => this.viewNext()}>
                     <ArrowIosForwardOutline size='48'/>
