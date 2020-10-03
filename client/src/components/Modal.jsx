40647f83cd4c32fb1cca19bc6f63f999c81fc38f
Ahmed-Elawad <a.elawad97@gmail.com>
2020-10-03 12:00:17 -0400

    Modal style refactor & functionality update

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,29 +1,43 @@
-import styled from 'styled-components';
+import Styled from 'styled-components';
 import React from 'react';
 
-const View = styled.div`
-    display:  block;
-    position: fixed;
-    border: solid 1px silver;
-    z-index: 1;
-    right: 0;
+const View = Styled.div`
+    display: flex;
+    position: absolute;
     top: 0;
-    height: 100%;
-    width: 35%;
-    overflow: auto;
-    background-color: white;
-    `;
+    right: 0;
+    bottom: 0;
+    background: #fff;
+    z-index: 9001;
+    overflow: hidden;
+    flex-shrink: 0;
+    text-align: left;
+    flex-direction: column;
+    width: 21.25rem;
+`;
 
-const Modal = ({Content, show}) => {
-return <View>{Content.map(Item => {
-    if (Item.section instanceof Function) {
-        if  (Item.props) {
-            return  <Item.section details={Item.props} />
-        }
-        return <Item.section />
-    }
-    return <div>{Item.section}</div>
-})}</View>
+const ExitPane = Styled.div`
+    padding: .625rem 1.0625rem;
+`;
+
+const ModalContent = Styled.div`
+    padding: 1.87rem 1.87rem 3.125rem;
+    overflow: auto;
+    height: 100%;
+`;
+const Modal = ({Content}) => {
+return <View>
+    <ExitPane>Exit the module here</ExitPane>
+    <ModalContent>
+        {Content.map(Item => {
+            if (Item.section instanceof Function) {
+                if  (Item.props) return  <Item.section details={Item.props}/>;
+                return <Item.section/>
+        } 
+        return <div>{Item.section}</div>
+    })}
+    </ModalContent>
+    </View>
 };
 
 
