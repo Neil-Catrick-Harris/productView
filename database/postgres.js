2e41186ca3793674df5476ea058287dd28273284
twzkraus <twzkraus@gmail.com>
2020-10-19 15:29:38 -0700

    updates postgres schema to include nested parameters of products

--- database/postgres.js
+++ database/postgres.js
@@ -5,8 +5,10 @@ const sequelize = new Sequelize('productviewdb', 'productview','pineapple', {
   dialect: 'postgres',
   logging: false
 });
+/*
+name,id,description,materials,sustainibility,packaging.shortDesc,packaging.measurments.width,packaging.measurments.height,packaging.measurments.length,packaging.measurments.weight,packaging.measurments.packages,sizes.fitting,sizes.attributes.thread-count,sizes.attributes.Pillowcase quantity,sizes.attributes.Duvet cover length,sizes.attributes.Duvet cover width,sizes.attributes.Pillowcase length,sizes.attributes.Pillowcase width,imageUrls*/
 
-const Product = sequelize.define('item',{
+const Product = sequelize.define('largeschematestitem',{
   name: {
     type: DataTypes.STRING,
     allowNull: false
@@ -29,19 +31,73 @@ const Product = sequelize.define('item',{
     type: DataTypes.STRING,
     allowNull: false
   },
-  packaging: {
-    type: DataTypes.JSON,
+  'packaging_shortdesc': {
+    type: DataTypes.STRING,
     allowNull: false
   },
-  sizes: {
-    type: DataTypes.JSON,
+  'packaging_measurments_width': {
+    type: DataTypes.STRING,
     allowNull: false
   },
-  imageUrls: {
-    type: DataTypes.JSON,
+  'packaging_measurments_height': {
+    type: DataTypes.STRING,
     allowNull: false
+  },
+  'packaging_measurments_length': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'packaging_measurments_weight': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'packaging_measurments_packages': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_fitting': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_attributes_threadcount': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_attributes_pillowcase_quantity': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_attributes_duvet_cover_length': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_attributes_duvet_cover_width': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_attributes_pillowcase_length': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  'sizes_attributes_pillowcase_width': {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  imageurls: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: true,
   }
 });
+
+sequelize.sync();
 
 const shutdownPostgres = sequelize.close;
 
