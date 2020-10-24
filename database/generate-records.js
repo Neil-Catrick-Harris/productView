66813f7b497c73d7cca7c8f3e2aa01404784a568
twzkraus <twzkraus@gmail.com>
2020-10-24 16:28:02 -0700

    removes postgres functionality from generate-records.js, optimizing for Cassandra-only usage

--- database/generate-records.js
+++ database/generate-records.js
@@ -40,14 +40,6 @@ let getImageIds = (n) => {
   return imageIds;
 };
 
-const writeImageRecords = async (document) => {
-  let categories = ["fashion", "nature", "abstract", "animals", "business", "cats", "city", "food", "nightlife"];
-  await document.write('id,url\n');
-  categories.forEach(async (category, i) => {
-    await document.write(`${i},${`http://placeimg.com/640/480/${category}`}\n`);
-  });
-};
-
 const generateRecord = (i) => {
   return {
     id: i,
@@ -71,20 +63,9 @@ const includeImageUrls = (record) => {
   return record;
 };
 
-const getImageRelations = (id) => {
-  let thruTableText = '';
-  getImageIds(id).forEach(imageId => {
-    thruTableText += `${id},${imageId}\n`;
-  });
-  return thruTableText;
-};
-
-const productDataSheet = fs.createWriteStream('./database/data-products.csv');
-const imageDataSheet = fs.createWriteStream('./database/data-images.csv');
-const throughSheet = fs.createWriteStream('./database/data-both.csv');
 const noSQLSheet = fs.createWriteStream('./database/data-full.csv');
 
-const writeNRecords = (n, document, callback) => {
+const writeNRecords = (n, callback) => {
   const startTime = new Date();
   let i = n;
   let id = 0;
@@ -92,42 +73,28 @@ const writeNRecords = (n, document, callback) => {
     verticalOutput: false,
     mapHeaders: (header) => '',
   }
-  throughSheet.write('itemsId,imagesId\n');
   const write = async () => {
-    let ok1 = true;
-    let ok2 = true;
-    while (i > 0 && ok1 && ok2) {
+    let ok = true;
+    while (i > 0 && ok) {
       i -= 1;
       id += 1;
       let rawRecord = generateRecord(id);
-      let record = await jsonexport(rawRecord, options);
       let intermediateRecord = includeImageUrls(rawRecord);
       let noSQLRecord = await jsonexport(intermediateRecord, {verticalOutput: false});
       if (id !== 1) {
-        record = record.split('\n').slice(1).join('');
         noSQLRecord = noSQLRecord.split('\n').slice(1).join('');
       }
-      record += '\n';
       noSQLRecord += '\n';
       if (i === 0) {
-        document.write(record, 'utf8', () => {
           noSQLSheet.write(noSQLRecord, 'utf8', () => {
-            throughSheet.write(getImageRelations(id), 'utf8', () => {
               console.log(`${id} records written in ${new Date() - startTime} ms`);
               callback(new Date());
-            });
           });
-        });
       } else {
-        ok1 = document.write(record, 'utf8');
-        ok2 = noSQLSheet.write(noSQLRecord, 'utf8');
-        throughSheet.write(getImageRelations(id), 'utf8');
+        ok = noSQLSheet.write(noSQLRecord, 'utf8');
       }
     }
-    if (i > 0 && !ok1) {
-      document.once('drain', write);
-    }
-    if (i > 0 && !ok2) {
+    if (i > 0 && !ok) {
       noSQLSheet.once('drain', write);
     }
   }
@@ -135,6 +102,5 @@ const writeNRecords = (n, document, callback) => {
 };
 
 module.exports = (n, callback) => {
-  writeImageRecords(imageDataSheet);
-  writeNRecords(n, productDataSheet, callback);
+  writeNRecords(n, callback);
 };
